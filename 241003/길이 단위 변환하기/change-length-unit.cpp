0aa329636b1924949bd9e3c8926f7e7b9ce2f628
kimmg19 <gkgkalsrms@naver.com>
2024-10-03 23:57:59 +0900

    Title: 길이 단위 변환하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241003/길이 단위 변환하기/change-length-unit.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float ft=30.48;
+    float mi=160934;
+    cout<<fixed;
+    cout.precision(1);
+    cout<<"9.2ft = "<<ft*9.2<<"cm"<<"\n";
+    cout<<"1.3mi = "<<mi*1.3<<"cm";
+
+    return 0;
+}
