3a92688ef44c903a1e0f0d342c7c090b6b46fff3
kimmg19 <gkgkalsrms@naver.com>
2024-10-03 23:53:14 +0900

    Title: 달에서 무게 구하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241003/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a=13;
+    float moon=0.165;
+    cout<<fixed;
+    cout.precision(6);
+    cout<<a<<" * "<<moon<<" = "<<a*moon;
+    return 0;
+}
