8724f5df7ff9c73bcfeb2885ef37e1e2e58e4497
kimmg19 <gkgkalsrms@naver.com>
2024-10-05 22:22:52 +0900

    Title: 살 수 있는 물건 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241005/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int money;
+    cin>>money;
+    if(money>=3000){
+        cout<<"book";
+    }else if(money>=1000&&money<3000){
+        cout<<"mask"
+    }else if(money>=500&&money<1000)
+        cout<<"pen";
+    else cout<<"no";
+    return 0;
+}
