aa37e986c844b857e9704fb78510314f1fb54b22
kimmg19 <gkgkalsrms@naver.com>
2024-10-05 21:46:10 +0900

    Title: 합과 평균의 차, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241005/합과 평균의 차/sub-of-average-and-sum.cpp
+++ 241005/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main() {
     int a,b,c;
     cin>>a>>b>>c;
-    cout<<a+b+c<<"\n "<<(a+b+c)/3<<"\n"<<(a+b+c)-((a+b+c)/3);
+    cout<<a+b+c<<"\n"<<(a+b+c)/3<<"\n"<<(a+b+c)-((a+b+c)/3);
     return 0;
 }
