cdf71b6200ca8fad4345d4e258cb7c07d807f574
kimmg19 <gkgkalsrms@naver.com>
2024-10-05 22:08:28 +0900

    Title: 살 수 있는 물건, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241005/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int money;
+    cin>>money;
+    if(money>=3000){
+        cout<<"book";
+    }else if(0<money<3000){
+        cout<<"mask";
+    }else cout<<"no";
+    return 0;
+}
