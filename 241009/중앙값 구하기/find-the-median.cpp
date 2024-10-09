6e77190733ea5b58a4b4aef06846ee4de5e449a8
kimmg19 <gkgkalsrms@naver.com>
2024-10-09 22:57:19 +0900

    Title: 중앙값 구하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241009/중앙값 구하기/find-the-median.cpp
+++ 241009/중앙값 구하기/find-the-median.cpp
@@ -6,16 +6,17 @@ int main() {
     cin>>a>>b>>c;
     if(a>c){
         if(b>a)cout<<a;
-        else{
-            if(b>c)cout<<b;
-            else cout<<c;
-        }
+        else if(b>c)cout<<b;
+        else cout<<c;
+            
+        
     }else{
-        if(a>b)cout<<b;
-        else{
-            if(a>c)cout<<a;
-            else cout<<c;
-        }
+        if(b>c)cout<<c;
+        else if(a>b)cout<<b;
+        else cout<<a;
     }
+        
+        
+    
     return 0;
 }
