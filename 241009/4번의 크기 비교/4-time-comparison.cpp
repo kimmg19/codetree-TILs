59683aa8e264516a28b262bae7a34674839df746
kimmg19 <gkgkalsrms@naver.com>
2024-10-09 21:58:40 +0900

    Title: 4번의 크기 비교, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241009/4번의 크기 비교/4-time-comparison.cpp
+++ 241009/4번의 크기 비교/4-time-comparison.cpp
@@ -5,17 +5,7 @@ int main() {
     int a,b,c,d,e;
     cin>>a;
     cin>>b>>c>>d>>e;
-    if(a>b)cout<<"1"<<"\n";
-    else cout<<"0"<<"\n";
-
-    if(a>c)cout<<"1"<<"\n";
-    else cout<<"0"<<"\n";
-
-    if(a>d)cout<<"1"<<"\n";
-    else cout<<"0"<<"\n";
-
-    if(a>e)cout<<"1"<<"\n";
-    else cout<<"0"<<"\n";
+    cout<<(a>b)<<"\n"<<(a>c)<<"\n"<<(a>d)<<"\n"<<(a>e)<<"\n"
 
     return 0;
 }
