7a44d4966b7b5168635a00734580bdd12af2c6e6
kimmg19 <gkgkalsrms@naver.com>
2024-10-09 22:29:49 +0900

    Title: 두 사람, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 241009/두 사람/two-person.cpp
+++ 241009/두 사람/two-person.cpp
@@ -5,7 +5,7 @@ int main() {
     int p1,p2;
     char sex1,sex2;
     cin>>p1>>sex1>>p2>>sex2;
-    cout<<((p1>=19||p2>19)&&(sex1=='M'||sex2=='M'));
+    cout<<((p1>=19&&sex1=='M')||(p2>19&&sex2=='M'));
     
     return 0;
 }
