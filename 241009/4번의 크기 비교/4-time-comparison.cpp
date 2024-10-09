e639d5b279d2366973a1f38172a9b309d3ac730a
kimmg19 <gkgkalsrms@naver.com>
2024-10-09 21:59:08 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241009/4번의 크기 비교/4-time-comparison.cpp
+++ 241009/4번의 크기 비교/4-time-comparison.cpp
@@ -5,7 +5,7 @@ int main() {
     int a,b,c,d,e;
     cin>>a;
     cin>>b>>c>>d>>e;
-    cout<<(a>b)<<"\n"<<(a>c)<<"\n"<<(a>d)<<"\n"<<(a>e)<<"\n"
+    cout<<(a>b)<<"\n"<<(a>c)<<"\n"<<(a>d)<<"\n"<<(a>e)<<"\n";
 
     return 0;
 }
