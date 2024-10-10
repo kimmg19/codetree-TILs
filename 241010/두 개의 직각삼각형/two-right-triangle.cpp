55aa25a1ce941d94d8281531fe96828fb08dd99d
kimmg19 <gkgkalsrms@naver.com>
2024-10-10 22:43:01 +0900

    Title: 두 개의 직각삼각형, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241010/두 개의 직각삼각형/two-right-triangle.cpp
+++ 241010/두 개의 직각삼각형/two-right-triangle.cpp
@@ -5,13 +5,13 @@ int main() {
     int n;
     cin>>n;
     for(int i=0;i<n;i++){
-        for(j=0;j<n;j++){
+        for(int j=0;j<n;j++){
             cout<<"*";
         }
-        for(k=0;k<i*2;k++){
+        for(int k=0;k<i*2;k++){
             cout<<" ";
         }
-        for(j=0;j<n;j++){
+        for(int j=0;j<n;j++){
             cout<<"*";
         }
         cout<<"\n";
