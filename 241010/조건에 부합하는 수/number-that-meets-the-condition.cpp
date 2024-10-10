9372bde77098f916c2250397bfd80fbe80b0e612
kimmg19 <gkgkalsrms@naver.com>
2024-10-10 21:04:04 +0900

    Title: 조건에 부합하는 수, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241010/조건에 부합하는 수/number-that-meets-the-condition.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin>>a;
+    for(int i=1;i<=a;i++){
+        if(i%2==0 && i%4!=0)continue;
+        if((i/8)%2==0)continue;
+        if((i%7)<4)continue;
+        cout<<i<<"\n";
+    }
+    return 0;
+}
