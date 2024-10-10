34c9b356fb0fb5f44811db38452ac64fbb228bb5
kimmg19 <gkgkalsrms@naver.com>
2024-10-10 21:20:29 +0900

    Title: 조건에 따른 연산, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241010/조건에 따른 연산/operatino-by-rule.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int cnt=0;
+    cin>>n;
+    while(n<1000){
+        if(n%2==0)n=n*3+1;
+        else n*2+2;
+        cnt++;
+    }
+    return 0;
+}
