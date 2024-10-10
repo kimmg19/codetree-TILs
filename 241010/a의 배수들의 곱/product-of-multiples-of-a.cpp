b980dbc25a33140331ebc975e115c3af33454000
kimmg19 <gkgkalsrms@naver.com>
2024-10-10 20:57:01 +0900

    Title: a의 배수들의 곱, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241010/a의 배수들의 곱/product-of-multiples-of-a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    int mlt=1;
+    for(int i=1;i<=b;i++){
+        if(i%2==0)mlt*=i;
+    }
+    cout<<mlt;
+    return 0;
+}
