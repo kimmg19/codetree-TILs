26f0b828b9e07e67314c43f330dea0e740e634b6
kimmg19 <gkgkalsrms@naver.com>
2024-10-10 22:11:56 +0900

    Title: 직각 삼각형 출력, Time: 3ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241010/직각 삼각형 출력/Right-triangle-and-output.cpp
+++ 241010/직각 삼각형 출력/Right-triangle-and-output.cpp
@@ -5,7 +5,7 @@ int main() {
     int n;
     cin>>n;
     for(int i=0;i<n;i++){
-        for(int k=1;k<=i;k++){
+        for(int k=1;k<=i*2-1;k++){
             cout<<"*";
         }
         cout<<"\n";
