cb344b6ed7c9c1aa1ca5bef7341c369e1ef1196f
kimmg19 <gkgkalsrms@naver.com>
2024-10-10 21:57:47 +0900

    Title: 공약수의 유무, Time: 2ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241010/공약수의 유무/presence-or-absence-of-a-common-divisor.cpp
+++ 241010/공약수의 유무/presence-or-absence-of-a-common-divisor.cpp
@@ -5,12 +5,10 @@ int main() {
     int a,b;
     cin>>a,b;
     for(int i=a;i<=b;i++){
-        if(1920%i==0&&2880%i==0){
+        if(1920%i==0 && 2880%i==0){
             cout<<"1";
             return 0;
-
         }
-
     }
     cout<<"0";
     return 0;
