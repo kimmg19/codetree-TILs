c06bcd2d7da9a3507a6c0238a509cc025052f155
kimmg19 <gkgkalsrms@naver.com>
2024-10-14 14:44:38 +0900

    Title: 구구단 만들기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241014/구구단 만들기 2/print-multiplication-table-2.cpp
+++ 241014/구구단 만들기 2/print-multiplication-table-2.cpp
@@ -7,7 +7,7 @@ int main() {
     for(int i=2;i<=8;i+=2){
         for(int j=b;j>=a;j--){
             cout<<j<<" * "<<i<<" = "<<i*j;
-            if(j>2)cout<<" / ";
+            if(j>a)cout<<" / ";
         }
         cout<<"\n";
         
