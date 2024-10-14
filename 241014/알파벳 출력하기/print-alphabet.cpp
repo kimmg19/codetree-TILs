7c991f196eac7d08abddf1eec03efa11a610192b
kimmg19 <gkgkalsrms@naver.com>
2024-10-14 17:39:08 +0900

    Title: 알파벳 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241014/알파벳 출력하기/print-alphabet.cpp
+++ 241014/알파벳 출력하기/print-alphabet.cpp
@@ -4,9 +4,11 @@ using namespace std;
 int main() {
     int n;
     char cnt='A';
+    cin>>n;
     for(int i=0;i<n;i++){
-        for(int j=i;j<n;j++){
-            cout<<(char)cnt++;
+        
+        for(int j=0;j<=i;j++){
+            cout<<cnt++;
         }
         cout<<"\n";
     }
