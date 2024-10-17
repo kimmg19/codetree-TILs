8aad0eb32cca30d6433dde9d0e4474353e1c9972
kimmg19 <gkgkalsrms@naver.com>
2024-10-17 18:04:50 +0900

    Title: 두 개의 격자 비교하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241017/두 개의 격자 비교하기/compare-two-grid.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,m,num;
+    cin>>n>>m;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr[i][j];
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>num;
+            if(num=arr[i][j])cout<<"0 ";
+            else cout<<"1 ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
