b39b15a56bcfd15c6df444f553c7e1de93024cfa
kimmg19 <gkgkalsrms@naver.com>
2024-10-17 19:03:33 +0900

    Title: 격자 반대로 채우기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241017/격자 반대로 채우기/grid-anti-fill.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,cnt=1;
+    cin>>n;
+    int arr[n][n];
+    for(int i=n-1;i>=0;i--){
+        if(i%2==0){
+            for(int j=n-1;j>=0;j--){
+                arr[j][i]=cnt++;
+        }else{
+            for(int j=0;j<n;j++){
+                arr[j][i]=cnt++;
+            }
+        }       
+        
+    }
+    for(int i=0;i<0;i++){
+        
+            for(int j=0;j<n;j++){
+                cout<<arr[i][j]<<" ";
+            }
+            cout<<"\n";
+    }
+    return 0;
+}
