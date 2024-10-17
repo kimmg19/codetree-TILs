e4f2dc5807e9646ce875e481163f69a045332804
kimmg19 <gkgkalsrms@naver.com>
2024-10-17 17:40:29 +0900

    Title: 배열의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241017/배열의 합/sum-of-array.cpp
+++ 241017/배열의 합/sum-of-array.cpp
@@ -10,7 +10,7 @@ int main() {
         }
     }
     for(int i=0;i<n;i++){
-        for(int j=0;j<nj++){
+        for(int j=0;j<n;j++){
             sum+=arr[i][j];
         }
         cout<<sum<<"\n";
