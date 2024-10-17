3ac115bd1b0150245354451f6c1f899df25f2226
kimmg19 <gkgkalsrms@naver.com>
2024-10-17 17:41:22 +0900

    Title: 배열의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241017/배열의 합/sum-of-array.cpp
+++ 241017/배열의 합/sum-of-array.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 int main() {
-    int arr[][];
+    int arr[4][4];
     int sum=0;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
             cin>>arr[i][j];
         }
     }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
             sum+=arr[i][j];
         }
         cout<<sum<<"\n";
