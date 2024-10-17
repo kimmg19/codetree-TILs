12c9c7a7354f45b28314e709684b7b986685ce50
kimmg19 <gkgkalsrms@naver.com>
2024-10-17 22:40:58 +0900

    Title: 격자 모양 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241017/격자 모양 출력하기/print-grid-shape.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,m.row,col;
+    cin>>n>>m;
+    int arr[n][n];
+    for(int i=0;i<m;i++){
+        cin>>col>>row;
+        arr[col-1][row-1]=col*row;
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<arr[i][j]<<" ";
+
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
