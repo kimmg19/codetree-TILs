9b125e74aae49be7df6dd1ad49b2e19430f81eb6
kimmg19 <gkgkalsrms@naver.com>
2024-10-21 22:06:42 +0900

    Title: 첫 번째와 두 번째 교환, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241021/첫 번째와 두 번째 교환/exchange-1st-and-2nd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str;
+    for(int i=0;i<str.length();i++){
+        if(str[i]==str[0])str[i]=str[1];
+        if(str[i]==str[1])str[i]=str[0];
+
+    }
+    cout<<str;
+    return 0;
+}
