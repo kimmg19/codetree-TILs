4978b189bee8959719ee743b56d867aab462f9fd
kimmg19 <gkgkalsrms@naver.com>
2024-10-21 18:45:40 +0900

    Title: 특정 문자의 유무, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241021/특정 문자의 유무/specific-character-presence.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    cin>>str;
+    bool isFind=false;
+    if(str.find("ee")==true)cout<<"Yes"<<" ";
+    else cout<<"No"<<" ";
+    for(int i=0;i<str.length()-1;i++){
+        if(str.substr(i,2)=="ab"){
+            isFind=true;
+            break;
+        }       
+    }
+    if(isFind==true)cout<<"Yes";
+    else cout<<"No";
+    return 0;
+}
