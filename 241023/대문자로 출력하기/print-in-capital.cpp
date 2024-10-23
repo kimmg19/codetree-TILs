21c7bdc1da511dfbe45ea501d09730ee9a802afb
kimmg19 <gkgkalsrms@naver.com>
2024-10-23 22:04:38 +0900

    Title: 대문자로 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241023/대문자로 출력하기/print-in-capital.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main() {
+    string str;
+    cin>>str;
+    for(int i=0;i<str.length();i++){
+        if(isalpha(str[i]))cout<<(char)(toupper(str[i]));
+    }
+    return 0;
+}
