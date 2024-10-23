f39913daa5f56009a7d5cfa11e873d2b80da12fd
kimmg19 <gkgkalsrms@naver.com>
2024-10-24 00:00:29 +0900

    Title: 미는 횟수, Time: 4ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241024/미는 횟수/number-of-pushes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string A;
+    string B;
+    int n=1;
+    cin>>A>>B;
+    for(int i=0;i<A.length()-1;i++){
+        A=A.substr(1,A.length()-1)+A.substr(0,1);
+        
+        if(A==B){cout<<n;return 0;}
+        else n++;
+    }
+        cout<<-1;
+    
+    return 0;
+}
