d6fa04da4abbecfa2096eca91da4e16f59140c86
kimmg19 <gkgkalsrms@naver.com>
2024-10-23 21:46:46 +0900

    Title: 아스키코드표 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241023/아스키코드표 맞추기/chart-of-ASCII.cpp
+++ 241023/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     int a,b,c,d,e;
-    cin>>a>>b>>C>>d>>e;
+    cin>>a>>b>>c>>d>>e;
     cout<<(char)a<<" "<<(char)b<<" "<<(char)c<<" "<<(char)d<<" "<<(char)e;
     return 0;
 }
