98910003c5e9e7ca06cb4a12a49725fc7fe57973
kimmg19 <gkgkalsrms@naver.com>
2024-10-23 22:07:43 +0900

    Title: 소문자와 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241023/소문자와 숫자/letter-and-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main() {
+    string str;
+    cin>>str;
+    for(int i=0;i<str.length();i++){
+        if(isdigit(str[i]))cout<<str[i];
+        else if(isalpha(str[i]))cout<<(char)(tolower(str[i]));
+        else continue;
+    }
+    return 0;
+}
