b9b52dbd5ac8e04fe42cc3595c7bf8e830b8f2b7
kimmg19 <gkgkalsrms@naver.com>
2024-11-04 15:23:03 +0900

    Title: 함수를 이용한 윤년 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241104/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.cpp
+++ 241104/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 using namespace std;
-string CheckYear(int year){
+bool CheckYear(int year){
     if(year%4==0){
-        return "true";
-    }
-    if(year%100==0 && year%400==0)return "true";
+        if(year%100==0){
+            if(year%400==0)return true;
+            else return false;
+        }
+        else return true;
+    }else return false;
     
-    return "false";
 }
 int main() {
     // 여기에 코드를 작성해주세요.
     int y;
     cin>>y;
-    cout<<CheckYear(y);
+    if(CheckYear(y))cout<<"true";
+    else cout<<"false";
     return 0;
 }
