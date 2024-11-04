36535c71431316e12eb71393d464d239f68106db
kimmg19 <gkgkalsrms@naver.com>
2024-11-04 14:31:04 +0900

    Title: 함수를 이용한 369 게임, Time: 23ms, Memory: 0MB, Status: Passed - CodeTree

--- 241104/함수를 이용한 369 게임/369-games-using-functions.cpp
+++ 241104/함수를 이용한 369 게임/369-games-using-functions.cpp
@@ -5,14 +5,14 @@ using namespace std;
 bool ThreeSixNine(int n){   
     string ntos=to_string(n);
     for(int i=0;i<ntos.length();i++){
-        if((ntos[i]%3==0)&&ntos[i]!=0) { return true;}
+        if(ntos[i]!='0' && (ntos[i]-'0')%3==0) {return true;}
     }
     return false;
 }
 int CheckNumber(int a,int b){
     int cnt=0;
     for(int i=a;i<=b;i++){
-        if(ThreeSixNine(i) || i%3==0){
+        if(ThreeSixNine(i) || (i%3==0)){
            
             cnt++;
         }
