234023c9dec3111b15756ce0b8ad1a7f429ca37a
kimmg19 <gkgkalsrms@naver.com>
2024-11-04 15:03:24 +0900

    Title: 함수를 이용한 소수 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241104/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
+++ 241104/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 
 bool Is_Prime(int num){
+    if(num==1)return false;
     for(int i=2;i<num;i++){
         if(num%i==0){            
             return false;
         }        
     }
-        return true;
+    return true;
 }
 int PrimeRange(int a,int b){
     int sum=0;
