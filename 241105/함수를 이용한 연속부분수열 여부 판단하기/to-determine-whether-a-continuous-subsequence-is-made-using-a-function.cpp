bb4d4db74ab1de4e9ebf7c48e8df49d19cdff828
kimmg19 <gkgkalsrms@naver.com>
2024-11-05 21:24:07 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 241105/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
+++ 241105/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
@@ -3,21 +3,25 @@ using namespace std;
 
 bool Continuous(int arr1[],int arr2[],int n1,int n2){
     bool isConti=false;
-    for(int i=0;i<n1-n2;i++){
+    for(int i=0;i<=n1-n2;i++){
+        
         if(arr1[i]==arr2[0]){
             for(int j=0;j<n2;j++){
-                if(arr1[i+j]==arr2[j])isConti=true;
+                if(arr1[i+j]==arr2[j]){
+                    isConti=true;
+                    // cout<<arr1[i+j]<<" "<<arr2[j];
+                }
                 else isConti=false;
-            }            
+            }
         }
-        else isConti=false;
+        else continue;
     }
     return isConti;
 }
 int main() {
     // 여기에 코드를 작성해주세요.
     int n1,n2;
-    cin>>n1,n2;
+    cin>>n1>>n2;
     int A[n1];
     int B[n2];
 
