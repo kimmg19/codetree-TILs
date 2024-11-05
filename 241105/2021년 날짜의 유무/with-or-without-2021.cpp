e9475443e62d7e2f7df4fcfd53312dc7e6ffe41c
kimmg19 <gkgkalsrms@naver.com>
2024-11-05 21:39:53 +0900

    Title: 2021년 날짜의 유무, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241105/2021년 날짜의 유무/with-or-without-2021.cpp
+++ 241105/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -2,19 +2,30 @@
 using namespace std;
 
 bool MonthDay(int n,int m){
-    
-    if(n<=12 && n%2==0){
-        if(n==2){
-            if(m<=28)return true;
-            else return false;
+    if(n>12)return false;
+    if(n<=7){
+        if(n%2==0){
+            if(n==2){
+                if(m<=28)return true;
+                else return false;
+            }else{
+                if(m<=30)return true;
+                else return false;
+            }
         }else{
-            if(m<=31)return true;
+            if(m<=31) return true;
             else return false;
-        }
-    }else if(n<=12 && n%2==1){
-        if(m<=30) return true;
-        else return false;
-    }else return false;
+        }  
+    }else {
+        if(n%2==0){
+            if(m<=31)return true;
+            else return false;           
+                
+        }else{
+            if(m<=30) return true;
+            else return false;
+    }
+      
 }
 int main() {
     // 여기에 코드를 작성해주세요.
