80e11d7ef7a710c3793e4ab5ed0781cd4d24d554
kimmg19 <gkgkalsrms@naver.com>
2024-11-05 21:31:50 +0900

    Title: 2021년 날짜의 유무, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241105/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+bool MonthDay(int n,int m){
+    
+    if(n<=12 && n%2==0){
+        if(n==2){
+            if(m<=28)return true;
+        }else{
+            if(m<=30)return true;
+            else return false;
+        }
+    }else if(n<=12 && n%2==1){
+        if(m<=31) return true;
+        else return false;
+    }else return false;
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int M,D;
+    cin>>M>>D;
+    if(MonthDay(M,D)){
+        cout<<"Yes";
+    }else cout<<"No";
+    return 0;
+}
