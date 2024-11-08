085e0dc0b567d2738f0f2e9aa7af221c24585a9e
kimmg19 <gkgkalsrms@naver.com>
2024-11-08 17:18:12 +0900

    Title: 반복 출력하기 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241108/반복 출력하기 2/repeated-output-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void HelloWorlld(int n){
+    if(n==0){
+        return;
+    }
+    cout<<"HelloWorld"<<endl;
+    HelloWorld(n-1);
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin>>n;
+    HelloWorlld(n);
+    return 0;
+}
