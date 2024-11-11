97f7722568cbabe66a83696d3b85e6d80a4c90d2
kimmg19 <gkgkalsrms@naver.com>
2024-11-11 15:28:09 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241111/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.cpp
+++ 241111/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.cpp
@@ -1,20 +1,13 @@
 #include <iostream>
 using namespace std;
 
-
-int Fnc_Even(int n){
-    if(n==0)return 0;
-    return n+Fnc_Even(n-2);
-}
-int Fnc_Odd(int n){
+int Fnc(int n){
     if(n==1)return 1;
-    return n+Fnc_Odd(n-2);
+    if(n==2)return 2;
+    return n+Fnc(n-2);
 }
-int main() {
-    // 여기에 코드를 작성해주세요.
+int main(){
     int n;
     cin>>n;
-    if(n%2==0)cout<<Fnc_Even(n);
-    else cout<<Fnc_Odd(n);
-    return 0;
+    cout<<Fnc(n);
 }
