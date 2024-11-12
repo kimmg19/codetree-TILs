bbaa0e0950224d4dc77b47b314e92325a4cd9115
kimmg19 <gkgkalsrms@naver.com>
2024-11-12 21:52:52 +0900

    Title: 이상한 수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241112/이상한 수열/a-strange-sequence.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int Fnc(int n){
+    if(n==1)return 1;
+    if(n==2)return 2;
+    return Fnc(n/3)+Fnc(n-1);
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin>>n;
+    cout<<Fnc(n);
+    return 0;
+}
