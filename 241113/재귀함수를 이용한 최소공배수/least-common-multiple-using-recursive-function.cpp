bf4bb0b9f029a802b869ff2caf6f5568602f1aee
kimmg19 <gkgkalsrms@naver.com>
2024-11-13 22:16:05 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241113/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.cpp
+++ 241113/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.cpp
@@ -4,23 +4,25 @@ using namespace std;
 int arr[10];
 int n;
 
-int Fnc(int num,int val){
-    if(val<2)return num;
-    while(){
-        if(num%arr[val-1]==0 && num%arr[val-2]==0)
-            return Fnc(num,val-1);
-        num++;
-    }
-    
+int Fnc(int num,int n){          
+
+    if(n<0)return num;
+    int val=num;
+    while(true){
+        if(val%arr[n]==0 && val%num==0){            
+            return Fnc(val,n-1);
+        }
+        val++;
+    }    
 }
 
 int main() {
     // 여기에 코드를 작성해주세요.    
     
     cin>>n;
-    for(int i=0;i<n;;i++){
+    for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    cout<<Fnc(arr[n-1],n);
+    cout<<Fnc(arr[n-1],n-1);
     return 0;
 }
