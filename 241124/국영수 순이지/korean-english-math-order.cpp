e54501a4d3a58845bb53120de34a0d81edc4cb10
kimmg19 <gkgkalsrms@naver.com>
2024-11-24 20:40:54 +0900

    Title: 국영수 순이지, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241124/국영수 순이지/korean-english-math-order.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+
+using namespace std;
+#define MAX 10
+
+class Student{
+    public :
+    string name;
+    int kor;
+    int eng;
+    int math;
+    Student(){};
+    Student(string name,int kor,int eng, int math){
+        this->name=name;
+        this->kor=kor;
+        this->eng=eng;
+        this->main=math;
+    }
+};
+
+bool compare(const Student &s1,const Student &s2){
+    if(s1.kor==s1.kor){
+        if(s1.eng==s1>eng) return s1.main>s2.math;
+        else return s1.eng>s2.eng;
+    }
+    return s1.kor>s2.kor;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin>>n;
+    Student student[MAX];
+    for(int i=0;i<n;i++){
+        string s_name;
+        int s_kor,s_eng,s_math;
+        cin>>s_name>>s_kor>>s_eng>>s_math;
+        student[i]=Student(s_name,s_kor,s_eng,s_math);
+    }
+
+    sort(student,student+n,compare);
+
+    for(int i=0;i<n;i++){
+        cout<<student[i].name<<" "<<student[i].kor<<" "<<student[i].eng<<" "<<student[i].math<<endl;
+    }
+    return 0;
+}
