4b6f167d176e0bd35e424058f7913bd032333600
kimmg19 <gkgkalsrms@naver.com>
2024-11-24 20:19:14 +0900

    Title: 키를 기준으로 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241124/키를 기준으로 정렬/sort-by-height.cpp
+++ 241124/키를 기준으로 정렬/sort-by-height.cpp
@@ -1,41 +1,28 @@
 #include <iostream>
 #include <string>
+#include <tuple>
 #include <algorithm>
 
 
 using namespace std;
 #define MAX 10
 
-class PersonData{
-    public :
-    string name;
-    int height;
-    int weight;
-    PersonData(){};
-    PersonData(string name,int height, int weight){
-        this->name=name;
-        this->height=height;
-        this->weight=weight;
-    }
-};
-bool compare(const PersonData &p1, const PersonData &p2){
-    return p1.height<p2.height;
-}
-
-int main() {
-    // 여기에 코드를 작성해주세요.
+int main(){
     int n;
     cin>>n;
-    PersonData personData[MAX];
+    tuple<int,string,int> personData[MAX];
+    for(int i=0;i<n;i++){
+        string name;
+        int height;
+        int weight;
+        cin>>name>>height>>weight;
+        personData[i]=make_tuple(height,name,weight);
+    }
+    sort(personData,personData+n);
     for(int i=0;i<n;i++){
         string name;
         int height,weight;
-        cin>>name>>height>>weight;
-        personData[i]=PersonData(name,height,weight);
+        tie(height,name,weight)=personData[i];
+        cout<<name<<" "<<height<<" "<<weight<<endl;
     }
-    sort(personData,personData+n,compare);
-    for(int i=0;i<n;i++){
-        cout<<personData[i].name<<" "<<personData[i].height<<" "<<personData[i].weight<<endl;
-    }
-    return 0;
 }
