bf5d675a8545e5bbac3df1e0cefd92ba82da035d
kimmg19 <gkgkalsrms@naver.com>
2024-11-26 12:04:18 +0900

    Title: 요일 맞추기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241126/요일 맞추기/guess-day-of-week.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+    int arr[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+
+int Fnc(int m,int d){
+    int day_of_month1=0;
+    for(int i=1;i<=m;i++){
+        if(i==m)day_of_month1+=d;
+
+        else day_of_month1+=arr[i];
+    } 
+    return day_of_month1;
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int m1,d1,m2,d2;
+    cin>>m1>>d1>>m2>>d2;
+    string day[7]={"Mon","Tue","Wed","Thu","Fri""Sat","Sun"};
+
+    int dayOfM1=Fnc(m1,d1);
+    int dayOfM2=Fnc(m2,d2);
+    cout<<day[(dayOfM2-dayOfM1)%7];    
+    return 0;
+}
