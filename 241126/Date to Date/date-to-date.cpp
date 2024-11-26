b1613dee5a064ac7b991e650b53d154996ef6ff9
kimmg19 <gkgkalsrms@naver.com>
2024-11-26 10:40:55 +0900

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241126/Date to Date/date-to-date.cpp
+++ 241126/Date to Date/date-to-date.cpp
@@ -12,13 +12,12 @@ int main() {
     for(int i=1;i<=m1;i++){
         if(i==m1)day_of_month1+=d1;
 
-        day_of_month1+=arr[i];
+        else day_of_month1+=arr[i];
     } 
     for(int i=1;i<=m2;i++){
         if(i==m2)day_of_month2+=d2;
-        day_of_month2+=arr[i];
+        else day_of_month2+=arr[i];
     }  
-    if(day_of_month1==day_of_month2)cout<<1;
-    else cout<<day_of_month2-day_of_month1;
+    cout<<day_of_month2-day_of_month1+1;
     return 0;
 }
