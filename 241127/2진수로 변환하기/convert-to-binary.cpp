7b02dd3a0611f8b22b44c350c387ba988e9afb99
kimmg19 <gkgkalsrms@naver.com>
2024-11-27 19:28:40 +0900

    Title: 2진수로 변환하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241127/2진수로 변환하기/convert-to-binary.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin>>n;
+    int arr[20]={};
+    int cnt=0;
+    while(true){
+        if(n==0)break;
+        arr[cnt]=n%2;
+        n/=2;
+        cnt++;
+    }
+    for(int i=cnt-1;i>=0;i--){
+        cout<<arr[i];
+    }
+    return 0;
+}
