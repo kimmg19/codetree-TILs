3a6dd151b3519ca13098fc597bf12c7aeb6d4ae2
kimmg19 <gkgkalsrms@naver.com>
2024-11-28 23:25:53 +0900

    Title: 최대로 겹치는 구간, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241128/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define MAX 200
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int arr[MAX]={};
+
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int x1,x2;
+        cin>>x1>>x2;
+        for(int j=x1;j<x2;j++){
+            arr[j]++;
+        }
+
+    }
+    cout<<*max_element(arr,arr+MAX);
+    return 0;
+}
