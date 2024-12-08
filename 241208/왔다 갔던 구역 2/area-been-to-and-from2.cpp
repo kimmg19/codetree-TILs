e5db458ef45e1d96aebadfb83de889be3ab961e7
kimmg19 <gkgkalsrms@naver.com>
2024-12-08 17:59:46 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241208/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int arr[2000]={};
+int pnt=1000;
+void Fnc(int x,char r){
+    if(r=='R'){
+        for(int i=0;i<x;i++){                
+            arr[pnt++]++;
+        }
+    }
+    else
+    {
+        for(int i=0;i<x;i++){                
+            arr[pnt--]++;
+        }
+    }    
+}
+
+int main() {
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int x;
+        char r;
+        cin>>x>>r;
+        Fnc(x,r);
+    }
+    int cnt=0;
+    for(int i=0;i<=2000;i++){
+        if(arr[i]>=2)cnt++;
+    
+    }
+    cout<<cnt;
+    return 0;
+
+}
