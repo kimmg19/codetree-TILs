ba93cbf655704436a02e14fec485f5b6fcd40a07
kimmg19 <gkgkalsrms@naver.com>
2024-12-09 22:48:10 +0900

    Title: 신기한 타일 뒤집기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241209/신기한 타일 뒤집기/strange-flipping-tiles.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+char arr[100000]={};
+int pnt=50000;
+void Fnc(int x, char r){
+    if(r=='R'){
+        for(int i=0;i<x;i++ ){
+            arr[pnt++]='B';
+        }
+    }else{
+        for(int i=0;i<x;i++ ){
+            arr[--pnt]='W';
+        }
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    int white=0;
+    int black=0;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int x;
+        char r;
+        cin>>x>>r;
+        Fnc(x,r);
+    }
+    for(int i=0;i<100000;i++){
+        if(arr[i]=='B')black++;
+        else if (arr[i]=='W')white++;
+    }
+    cout<<white<<" "<<black;
+    return 0;
+}
