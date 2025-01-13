8f4829acd262d2298adf8b0857c1fee442b2b361
김민근 <gkgkalsrms@naver.com>
2025-01-14 00:12:21 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250114/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+#define NMAX 99
+#define TMAX 250
+class Developer{
+    public :
+    int shakeAmount;
+    bool isInfection;
+    Developer(){
+        this->isInfection=false;
+        this->shakeAmount=0;
+    }
+    Developer(int shakeAmount){
+        this->shakeAmount=shakeAmount;
+        this->isInfection=false;
+    }
+};
+class ShakeInfo{
+    public:
+    int x,y;
+    ShakeInfo(){
+        this->x=0;
+        this->y=0;
+    }
+    ShakeInfo(int x,int y){
+        this->x=x;
+        this->y=y;
+    }
+};
+
+Developer developer[NMAX]={};
+ShakeInfo shakeInfo[TMAX]={};
+
+void Shaking(){
+    for(int i=0;i<TMAX;i++){
+        if(shakeInfo[i].x==0)continue;
+        if(developer[shakeInfo[i].x-1].shakeAmount==0 || developer[shakeInfo[i].y-1].shakeAmount==0)continue;
+        if(developer[shakeInfo[i].x-1].isInfection || developer[shakeInfo[i].y-1].isInfection){
+            developer[shakeInfo[i].x-1].isInfection=true;
+            developer[shakeInfo[i].y-1].isInfection=true;
+            developer[shakeInfo[i].x-1].shakeAmount--;
+            developer[shakeInfo[i].y-1].shakeAmount--;
+        }        
+    }
+}
+
+
+int main() {
+    // Please write your code here.
+    int N,K,P,T;   
+
+    cin>>N>>K>>P>>T;
+    for(int i=0;i<N;i++){
+            developer[i]=Developer(K);
+    }
+
+    developer[P].isInfection=true;
+
+    for(int i=0;i<T;i++){
+        int t,x,y;
+        cin>>t>>x>>y;
+        shakeInfo[t-1]=ShakeInfo(x,y);
+    }
+    Shaking();
+    for(int i=0;i<N;i++){
+        cout<<developer[i].isInfection;
+    }
+    return 0;
+}
