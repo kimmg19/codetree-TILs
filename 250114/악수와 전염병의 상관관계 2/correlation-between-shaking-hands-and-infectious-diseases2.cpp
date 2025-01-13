f812769ecc557149ab77b780ff4e9b1c11f655e3
김민근 <gkgkalsrms@naver.com>
2025-01-14 01:13:42 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250114/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
+++ 250114/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
@@ -35,26 +35,26 @@ void Shaking(){
     for(int i=0;i<TMAX;i++){
         if(shakeInfo[i].x==0)continue;  
 
-        // if(developer[shakeInfo[i].x-1].isInfection && developer[shakeInfo[i].x-1].shakeAmount>0){          
-        //     if(developer[shakeInfo[i].y-1].isInfection) developer[shakeInfo[i].y-1].shakeAmount--;
-        //     developer[shakeInfo[i].y-1].isInfection=true;
-        //     developer[shakeInfo[i].x-1].shakeAmount--;
-        // }
-        // else if(developer[shakeInfo[i].y-1].isInfection && developer[shakeInfo[i].y-1].shakeAmount>0){            
-        //     if(developer[shakeInfo[i].x-1].isInfection) developer[shakeInfo[i].x-1].shakeAmount--;
-        //     developer[shakeInfo[i].x-1].isInfection=true;
-        //     developer[shakeInfo[i].y-1].shakeAmount--;
-        //     cout<<" "<<developer[shakeInfo[i].y-1].shakeAmount<<endl;
-        // }
-        // else continue;
+        if(developer[shakeInfo[i].x-1].isInfection && developer[shakeInfo[i].x-1].shakeAmount>0){          
+            if(developer[shakeInfo[i].y-1].isInfection) developer[shakeInfo[i].y-1].shakeAmount--;
+            developer[shakeInfo[i].y-1].isInfection=true;
+            developer[shakeInfo[i].x-1].shakeAmount--;
+        }
+        else if(developer[shakeInfo[i].y-1].isInfection && developer[shakeInfo[i].y-1].shakeAmount>0){            
+            if(developer[shakeInfo[i].x-1].isInfection) developer[shakeInfo[i].x-1].shakeAmount--;
+            developer[shakeInfo[i].x-1].isInfection=true;
+            developer[shakeInfo[i].y-1].shakeAmount--;
+            // cout<<i<<" "<<developer[shakeInfo[i].y-1].shakeAmount<<endl;
+        }
+        else continue;
 
-         if(developer[shakeInfo[i].x-1].isInfection || developer[shakeInfo[i].y-1].isInfection){
-             if(developer[shakeInfo[i].x-1].shakeAmount==0 || developer[shakeInfo[i].y-1].shakeAmount==0)continue;
-              developer[shakeInfo[i].x-1].isInfection=true;
-              developer[shakeInfo[i].y-1].isInfection=true;
-              developer[shakeInfo[i].x-1].shakeAmount--;
-              developer[shakeInfo[i].y-1].shakeAmount--;
-         }        
+        //  if(developer[shakeInfo[i].x-1].isInfection || developer[shakeInfo[i].y-1].isInfection){
+        //      if(developer[shakeInfo[i].x-1].shakeAmount==0 || developer[shakeInfo[i].y-1].shakeAmount==0)continue;
+        //       developer[shakeInfo[i].x-1].isInfection=true;
+        //       developer[shakeInfo[i].y-1].isInfection=true;
+        //       developer[shakeInfo[i].x-1].shakeAmount--;
+        //       developer[shakeInfo[i].y-1].shakeAmount--;
+        //  }        
     }
 }
 
@@ -68,7 +68,7 @@ int main() {
             developer[i]=Developer(K);
     }
 
-    developer[P].isInfection=true;
+    developer[P-1].isInfection=true;
 
     for(int i=0;i<T;i++){
         int t,x,y;
