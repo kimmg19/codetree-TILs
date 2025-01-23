be7613dd77ec0988cf2ad12844fba385dad28a59
김민근 <gkgkalsrms@naver.com>
2025-01-24 02:10:22 +0900

    Title: 작은 구슬의 이동, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250124/작은 구슬의 이동/small-marble-movement.cpp
+++ 250124/작은 구슬의 이동/small-marble-movement.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int n;
 
 bool IsOutOfRange(int x, int y){
-    return x<1 || x>=n || y <1|| y>=n;
+    return x<1 || x>n || y <1|| y>n;
 }
 int Direction(char d){
     if(d=='U'){
         return 0;
     }else if(d=='R'){
         return 1;
-    }else if(d=='L'){
+    }else if(d=='D'){
         return 2;
     }else{
         return 3;
@@ -26,10 +26,11 @@ int main() {
     cin>>n>>t;
     cin>>r>>c>>d;
 
-    int dx[4]={0,1,0,-1};
-    int dy[4]={1,0,-1,0};
+    int dx[4]={1,0,-1,0};
+    int dy[4]={0,1,0,-1};
 
     cur_dir=Direction(d);
+    // cout<<cur_dir<<endl;
     for(int i=1;i<=t;i++){ 
                
         if(IsOutOfRange(r+dx[cur_dir],c+dy[cur_dir])){
@@ -39,8 +40,10 @@ int main() {
         }
         r=r+dx[cur_dir];
         c=c+dy[cur_dir];
+        // cout<<i<<" "<<r<<" "<<c<<endl;
         
     }
+    
     cout<<r<<" "<<c;
     return 0;
 }
