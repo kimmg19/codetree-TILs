1c0bd9a67b7009f96d0bccdb558fef476e33cb6c
김민근 <gkgkalsrms@naver.com>
2025-01-25 02:00:17 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250125/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
+++ 250125/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
@@ -4,33 +4,35 @@ using namespace std;
 
 int n,m;
 
-bool OutOfRange(int x, int y) {
-    return (x<0 || x >= n || y<0 || y >= m);
+bool IsRange(int x,int y){
+    
+    return (x>=0 && x<n && y>=0 && y<m);
 }
+
 int main() {
     // Please write your code here.
-    int arr[MAX][MAX]={0};
-    
+    int arr[MAX][MAX]={};
+  
     cin>>n>>m;
     int dx[4]={0,1,0,-1};
     int dy[4]={1,0,-1,0};
     int dir_num=0;
     int x=0,y=0;
-    arr[x][y] = 1;
-    for(int i=2;i<=n*m;i++){
+    for(int i=1;i<=n*m;i++){
         int nx=x+dx[dir_num];
         int ny=y+dy[dir_num];
-        if(OutOfRange(nx, ny) || arr[nx][ny] != 0){
-            dir_num = (dir_num + 1) % 4;  
+        if(!IsRange(nx,ny)||arr[nx][ny]!=0){
+            dir_num=(dir_num+1)%4;    
             
-        }
-            
-        x = x + dx[dir_num]; y = y + dy[dir_num];       
-        arr[x][y] = i;
+        }   
+        arr[x][y]=i;
+        x=x+dx[dir_num];
+        y=y+dy[dir_num];
     }
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
             cout<<arr[i][j]<<" ";
+
         }
         cout<<endl;
     }
