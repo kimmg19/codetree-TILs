dc23f8db88ae01da235dcf9ec1caa17742f5ad6e
김민근 <gkgkalsrms@naver.com>
2025-02-03 12:06:44 +0900

    Title: 최고의 13위치, Time: 14ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 250203/최고의 13위치/best-place-of-13.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define MAX 18
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+    int arr[MAX][MAX];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>arr[i][j];
+        }
+    }
+    int cnt=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-2;j++){
+            cnt= max(cnt,arr[i][j]+arr[i][j+1]+arr[i][j+2]);
+        }
+    }
+    cout<<cnt;
+    return 0;
+}
