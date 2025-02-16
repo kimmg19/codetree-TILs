7d962bd8f96316fdde3092508d876428dde307cf
김민근 <gkgkalsrms@naver.com>
2025-02-16 15:23:19 +0900

    Title: G or H 3, Time: 29ms, Memory: 0MB, Status: Passed - CodeTree

--- 250216/G or H 3/G-or-H-3.cpp
+++ 250216/G or H 3/G-or-H-3.cpp
@@ -7,16 +7,18 @@ int main() {
     int n,k;
     cin>>n>>k;
     int maxpoint=0;
-    char arr[10001];
+    char arr[10001]={};
     for(int i=0;i<n;i++){
         int point;
         char chr;
         cin>>point>>chr;
         maxpoint=max(point,maxpoint);
         arr[point]=chr;
+        // cout<<point<<" "<<arr[point]<<endl;
     }
+    
     int maxSum=0;
-    for(int i=1;i<=maxpoint-k;i++){
+    for(int i=1;i<=10000-k;i++){
         int sum=0;
         for(int j=i;j<=i+k;j++){
             if(arr[j]=='G'){
