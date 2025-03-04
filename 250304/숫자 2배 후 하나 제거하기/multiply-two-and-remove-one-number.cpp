48a5d4545232972780077f21390941988314a8bc
김민근 <gkgkalsrms@naver.com>
2025-03-04 21:43:03 +0900

    Title: 숫자 2배 후 하나 제거하기, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 250304/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.cpp
+++ 250304/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include<algorithm>
+#include<climits>
 using namespace std;
 
 int main() {
@@ -9,17 +11,25 @@ int main() {
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    int mincnt=100;
-    for(int i=0;i<n;i++){
+    int mincnt=INT_MAX;
+    for(int i=0;i<n;i++){//2배하는 수
         arr[i]*=2;
-        for(int j=0;j<n;j++){
-            int sum=0;
-            for(int k=0;k<n-1;k++){
-                if(j==k)continue;
-                sum+=abs(arr[k]-arr[k+1]);
-            }
-            mincnt=min(sum,mincnt);
-        }
+        int remain[100]={};   
+        int num=0;     
+            for(int j=0;j<n;j++){  //제외할 수
+                for(int k=0;k<n;k++){//2배와 제외한 수를 뺴고 문제 실행.
+                    if(j==k)continue;
+                    remain[num++]=arr[k];
+                }     
+                int sum=0;
+
+                for(int h=0;h<n-2;h++){
+                    // cout<<"h: "<<remain[h]<<"h+1: "<<remain[h+1]<<endl;
+                    sum+=abs(remain[h]-remain[h+1]);
+            }      
+            // cout<<"=="<<endl;
+            mincnt=min(sum,mincnt);   
+            }        
         arr[i]/=2;
     }
     cout<<mincnt;
