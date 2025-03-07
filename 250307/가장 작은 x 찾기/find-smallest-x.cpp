830bc7b5fc13a0bfa640f3170620546a4eda613f
김민근 <gkgkalsrms@naver.com>
2025-03-07 17:58:57 +0900

    Title: 가장 작은 x 찾기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250307/가장 작은 x 찾기/find-smallest-x.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+int n;
+int a[10];
+int b[10];
+
+bool Check(int x){
+    for(int i=0;i<n;i++){
+        x*=2;
+        if(a[i]>x || x>b[i]){
+            return false;
+        }
+    }
+    return true;
+}
+int main() {
+// Please write your code here.
+cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>a[i]>>b[i];
+    }
+    int minnum=10000;
+    for(int x=1;x<10;x++){
+        if(Check(x)){
+            minnum=min(minnum,x);
+        }
+        
+    }
+    cout<<minnum;
+    return 0;
+}
