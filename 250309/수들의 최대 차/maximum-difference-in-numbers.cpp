894568b73e906e35e2f27d431bbfb5c1dd7b3f08
김민근 <gkgkalsrms@naver.com>
2025-03-09 17:44:47 +0900

    Title: 수들의 최대 차, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 250309/수들의 최대 차/maximum-difference-in-numbers.cpp
+++ 250309/수들의 최대 차/maximum-difference-in-numbers.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<algorithm>
 using namespace std;
 int arr[1000];
 
@@ -12,10 +13,9 @@ int main() {
     int maxcnt=0;
     for(int i=0;i<n;i++){
         int cnt=0;
-        int x=arr[i];
-        for(int j=0;j<n;j++){
-            if(i==j)continue;
-            if(abs(arr[j]-x)<=3){
+        int x=arr[i];//임의의 최솟값.
+        for(int j=0;j<n;j++){            
+            if(x>=arr[j]&&x-arr[j]<=k){
                 cnt++;
             }
         }
